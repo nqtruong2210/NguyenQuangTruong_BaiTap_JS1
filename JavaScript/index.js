86044a8227b1b4202830ed2aff77ad284c64c3c7
QuangTruong <nqtruong2210@gmail.com>
2023-08-02 23:51:31 +0700

    init project

---
+++ JavaScript/index.js
@@ -0,0 +1,57 @@
+//Bài 1: Tính tiền lương nhân viên
+var oneDaySalary,numberOfDays,totalSalary;
+oneDaySalary = '100000';
+numberOfDays = 3;
+
+totalSalary = oneDaySalary * 1 * numberOfDays;
+
+console.log('totalSalary: ', totalSalary);
+
+//Bài 2: Tính giá trị trung bình
+ var a, b, c, d, e, averageValue;
+ a = 1;
+ b = 2;
+ c = 3;
+ d = 4;
+ e = 5;
+
+ averageValue= (a + b + c + d + e) / 5;
+
+ console.log('averageValue = ', averageValue);
+
+//Bài 3: Quy đổi tiền
+ const USD = 2;
+ var vnd = 23500;
+ var usdToVnd;
+
+ usdToVnd = USD * vnd;
+
+ console.log('usdToVnd: ', usdToVnd);
+
+
+//Bài 4: Tính diện tích, chu vi hình chữ nhật
+ var length,width,acreage,perimeter;
+ length = 1;
+ width = 2;
+
+ acreage = length * width;
+ perimeter = (length + width) * 2;
+
+ console.log('acreage: ', acreage);
+ console.log('perimeter: ', perimeter);
+
+
+//Bài 4: Tính tổng 2 ký số
+ var a,units,ten,sum;
+ a=12;
+
+ units = Math.floor(a % 10);
+ ten = Math.floor(a / 10);
+ sum = Math.floor(a % 10) + Math.floor(a / 10);
+ 
+ console.log('units: ', units);
+ console.log('ten: ', ten);
+ console.log('sum: ', sum);
+ 
+
+
